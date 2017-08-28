be84b59a1e91d4c6055d36a94812389887eff260
bernh <bernhard.scharrer@gmx.net>
2017-08-28 19:48:31 +0200

    implemented login report

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,15 +1,28 @@
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
 
 public class MainFrame extends JFrame {
 	
 	private JPanel contentPane;
+	private JTextField username_field;
+	private JPasswordField password_field;
 
 	public MainFrame() {
 		try {
@@ -26,22 +39,74 @@ public class MainFrame extends JFrame {
 	 * @throws java.lang.Exception
 	 */
 	private void FrameInit() throws Exception {
+		
 		contentPane = (JPanel) getContentPane();
-		contentPane.setLayout(new GridLayout(3,2));
+		contentPane.setLayout(new BorderLayout());
 		super.setSize(new Dimension(400, 300));
 		super.setTitle("Frame Title");
 		
-		JLabel username_label = new JLabel("Username:");
-		contentPane.add(username_label);
+		EmptyBorder border1 = new EmptyBorder(35, 40, 40, 10);
+		EtchedBorder border2 = new EtchedBorder();
+		CompoundBorder cborder = BorderFactory.createCompoundBorder(border1, border2);
 		
-		JTextField username_field = new JTextField();
-		contentPane.add(username_field);
+		JPanel login_report = new JPanel(new GridLayout(2,2));
+		login_report.setBackground(Color.WHITE);
+		
+		JLabel username_label = new JLabel("Username:");
+		username_label.setBorder(border1);
+		login_report.add(username_label);
+		
+		username_field = new JTextField();
+		username_field.setBorder(cborder);
+		login_report.add(username_field);
 		
 		JLabel password_label = new JLabel("Password:");
-		contentPane.add(password_label);
+		password_label.setBorder(border1);
+		login_report.add(password_label);
 		
-		JTextField password_field = new JTextField();
-		contentPane.add(password_field);
+		password_field = new JPasswordField();
+		password_field.setBorder(cborder);
+		login_report.add(password_field);
+		
+		JPanel control_buttons = new JPanel(new FlowLayout());
+		
+		JButton login = new JButton("LOGIN");
+		LoginListener llistener = new LoginListener();
+		login.addActionListener(llistener);		
+		control_buttons.add(login);
+		
+		JButton cancel = new JButton("CANCEL");
+		CancelListener clistener = new CancelListener();
+		cancel.addActionListener(clistener);
+		control_buttons.add(cancel);
+		
+		contentPane.add(login_report, BorderLayout.CENTER);
+		contentPane.add(control_buttons, BorderLayout.SOUTH);
 		
 	}
+	
+	private class CancelListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.exit(0);
+		}
+		
+	}
+	
+	private class LoginListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			
+			char[] chars = password_field.getPassword();
+			String password = new String(chars);
+			
+			System.out.println(username_field.getText() + "|" + password);
+			
+		}
+		
+	}
+	
 }
